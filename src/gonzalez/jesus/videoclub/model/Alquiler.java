93090948b17b64bee487995b5365630d674ea474
JesusDevBoot <carballog10@outlook.com>
2026-06-15 22:39:52 +0200

    Terminado la clase Alquiler

--- src/gonzalez/jesus/videoclub/model/Alquiler.java
+++ src/gonzalez/jesus/videoclub/model/Alquiler.java
@@ -1,5 +1,77 @@
 package gonzalez.jesus.videoclub.model;
 
-public class Alquiler {
+import java.time.LocalDate;
 
+public class Alquiler {
+	
+	private final Cliente cliente;
+	private final Pelicula pelicula;
+	private final LocalDate fechaAlquiler;
+	private boolean devuelta;
+	private LocalDate fechaDevolucion;
+	
+	public Alquiler(Cliente cliente, Pelicula pelicula, LocalDate fechaAlquiler) {
+
+		if (cliente == null) {
+			throw new IllegalArgumentException("El cliente es obligatorio");
+		}
+		if (pelicula == null) {
+			throw new IllegalArgumentException("La película es obligatoria");
+		}
+		if (fechaAlquiler == null) {
+			throw new IllegalArgumentException("La fecha de alquiler es obligatoria");
+		}
+
+		this.cliente = cliente;
+		this.pelicula = pelicula;
+		this.fechaAlquiler = fechaAlquiler;
+		this.devuelta = false;
+	}
+
+	public LocalDate getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+	
+	public Cliente getCliente() {
+	    return cliente;
+	}
+		
+
+	public Pelicula getPelicula() {
+	    return pelicula;
+	}
+
+	public LocalDate getFechaAlquiler() {
+	    return fechaAlquiler;
+	}
+
+	public boolean isDevuelta() {
+	    return devuelta;
+	}
+	
+	public void devolver() {
+
+	    if(devuelta) {
+	        throw new IllegalStateException(
+	                "La película ya ha sido devuelta");
+	    }
+
+	    this.devuelta = true;
+	    this.fechaDevolucion = LocalDate.now();
+	}
+	
+	public double calcularPrecioFinal() {
+
+	    double precioBase = pelicula.calcularPrecio();
+
+	    double descuento =
+	            precioBase *
+	            cliente.getTipoCliente().getDescuento();
+
+	    return precioBase - descuento;
+	}
+	
+	
+	
+	
 }
--- src/gonzalez/jesus/videoclub/service/PeliculaService.java
+++ src/gonzalez/jesus/videoclub/service/PeliculaService.java
@@ -1,5 +1,16 @@
 package gonzalez.jesus.videoclub.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import gonzalez.jesus.videoclub.model.Pelicula;
+
 public class PeliculaService {
+	
+	private List<Pelicula> peliculas;
+	
+	public PeliculaService() {
+		this.peliculas = new ArrayList<>();
+	}
 
 }
